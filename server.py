21d326d1972880f29c755ed750b63aaef8664384
kwa_laa <khoaddnguyen@gmail.com>
2023-12-27 16:16:32 -0600

    add html files

--- server.py
+++ server.py
@@ -8,5 +8,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
+
+@app.route("/play")
+def play():
+    return render_template("play.html")
+
+@app.route("/fm")
+def fm():
+    return render_template("fm.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
